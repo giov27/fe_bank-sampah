f65f2e9cda36f47d1aed4c1d0f1a3c56e81ff0f6
giov27 <giovannimaendra@gmail.com>
2023-06-16 16:23:42 +0700

    first commit

---
+++ src/config/service.config.tsx
@@ -0,0 +1,12 @@
+
+
+// export const SERVICES_URL = "http://12.12.1.43:3000/api/v1"
+// export const SERVICES_URL = "http://192.168.110.18:3000/api/v1"
+export const SERVICES_URL = "http://localhost:3001/api/v1"
+
+
+// GARBAGE
+export const GARBAGE_LIST = SERVICES_URL + "/garbage/list"
+
+// AUTH
+export const LOGIN_USER = SERVICES_URL + "/auth/login"
---
+++ src/layouts/full/sidebar/MenuItems.tsx
@@ -0,0 +1,82 @@
+import {
+  IconAperture, IconCopy, IconLayoutDashboard, IconLogin, IconMoodHappy, IconTypography, IconUserPlus,
+  IconUsers, IconRecycleOff
+} from '@tabler/icons-react';
+
+import { uniqueId } from 'lodash';
+
+const Menuitems = [
+  {
+    navlabel: true,
+    subheader: 'Home',
+  },
+
+  {
+    id: uniqueId(),
+    title: 'Dashboard',
+    icon: IconLayoutDashboard,
+    href: '/',
+  },
+  {
+    id: uniqueId(),
+    title: 'Users',
+    icon: IconUsers,
+    href: '/users',
+  },
+  {
+    id: uniqueId(),
+    title: 'Garbage',
+    icon: IconRecycleOff,
+    href: '/garbage',
+  },
+  {
+    navlabel: true,
+    subheader: 'Utilities',
+  },
+  {
+    id: uniqueId(),
+    title: 'Typography',
+    icon: IconTypography,
+    href: '/utilities/typography',
+  },
+  {
+    id: uniqueId(),
+    title: 'Shadow',
+    icon: IconCopy,
+    href: '/utilities/shadow',
+  },
+  {
+    navlabel: true,
+    subheader: 'Auth',
+  },
+  {
+    id: uniqueId(),
+    title: 'Login',
+    icon: IconLogin,
+    href: '/authentication/login',
+  },
+  {
+    id: uniqueId(),
+    title: 'Register',
+    icon: IconUserPlus,
+    href: '/authentication/register',
+  },
+  {
+    navlabel: true,
+    subheader: 'Extra',
+  },
+  {
+    id: uniqueId(),
+    title: 'Icons',
+    icon: IconMoodHappy,
+    href: '/icons',
+  },
+  {
+    id: uniqueId(),
+    title: 'Sample Page',
+    icon: IconAperture,
+    href: '/sample-page',
+  },
+];
+
+export default Menuitems;
